bdaf119ab64be3c91321287c0b2672ea72236b4b
sarahmendes-ufca <sarah.mendes@aluno.ufca.edu.br>
2025-11-17 21:10:08 -0300

    Atualizar Leitura

--- app/Leitura.py
+++ app/Leitura.py
@@ -1,10 +1,15 @@
-from enum import Enum
+rom enum import Enum
+
+'''Classe Leitura engloba o status do livro, o início e o término da leitura'''
 
 class Leitura(Enum):
     NAO_LIDO = "NÃO LIDO"
     LENDO = "LENDO"
     LIDO = "LIDO"
 
+    def status_leitura(self):
+        pass 
+    
     def iniciar_leitura(self):
         pass
 
