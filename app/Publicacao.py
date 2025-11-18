8fc4e5961d7d4dd811b773eb914a0c0d982b10c2
sarahmendes-ufca <sarah.mendes@aluno.ufca.edu.br>
2025-11-17 21:07:51 -0300

    Atualizar Publicacao

--- app/Publicacao.py
+++ app/Publicacao.py
@@ -2,52 +2,43 @@ from datetime import date
 from typing import List
 from typing import Optional
 
+'''A Classe Publicação é a responsável por ser a base da criação de um livro. Origina duas subclasses: Livro e Revista '''
 class Publicacao:
-    def __init__(self, id_publicacao, titulo, autor, ano, tipo, genero, numero_de_paginas, nota_avaliacao, status_leitura, avaliacao, data_inclusao, data_inicio, data_fim, anotacoes):      
+    def __init__(self, id_publicacao, titulo, autor, ano, tipo, genero, numero_de_paginas, nota_avaliacao, status_leitura, avaliacao):      
         self.id_publicacao = id_publicacao
-        titulo = str
-        autor = str
-        ano = int
-        tipo = str
-        genero = str
-        numero_de_paginas = int
-        nota_avaliacao = int
-        status_leitura = str
-        avaliacao = Optional[float]
-        data_inclusao = date
-        data_inicio = Optional[date]
-        data_fim = Optional[date]
-        anotacoes = Optional[List]
-
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.tipo = tipo
+        self.genero = genero
+        self.numero_de_paginas = numero_de_paginas
+        self.nota_avaliacao = nota_avaliacao
+        self.status_leitura = status_leitura
+        self.avaliacao = avaliacao
+        
     @property
     def titulo(self):
-        return self._titulo
+        pass
 
     @titulo.setter
-    def titulo(self, valor: str):
-        if not valor or not valor.strip():
-            raise ValueError("O título não pode ser vazio.")
-        self._titulo = valor
+    def titulo(self, valor = str):
+        pass
 
     @property
     def ano(self):
-        return self._ano
+        pass
 
     @ano.setter
     def ano(self, valor: int):
-        if valor < 1500:
-            raise ValueError("O ano deve ser maior ou igual a 1500.")
-        self._ano = valor
+        pass
 
     @property
     def nota_avaliacao(self):
-        return self._nota_avaliacao
+        pass
 
     @nota_avaliacao.setter
     def nota_avaliacao(self, valor: float):
-        if not (0 <= valor <= 10):
-            raise ValueError("A nota deve estar entre 0 e 10.")
-        self._nota_avaliacao = valor
+        pass
 
     def adicionar_publicacao(self):
         pass
@@ -64,18 +55,6 @@ class Publicacao:
     def avaliar_publicacao(self, valor):
         pass
 
-    def iniciar_leitura(self):
-        pass
-
-    def concluir_leitura(self):
-        pass
-
-    def adicionar_anotacao(self, texto: str, trecho: Optional[str] = None):
-        pass
-
-    def listar_anotacoes(self):
-        pass
-
     def __str__(self):
         pass
 
@@ -87,3 +66,9 @@ class Publicacao:
 
     def __eq__(self, other):
         pass
+
+class Livro(Publicacao):
+    pass
+
+class Revista(Publicacao):
+    pass
