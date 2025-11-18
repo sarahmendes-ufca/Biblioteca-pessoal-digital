fb2b8013ecf2cb43e2be0fedbaf165c53f21ce0a
sarahmendes-ufca <sarah.mendes@aluno.ufca.edu.br>
2025-11-17 21:08:39 -0300

    Atualizar Anotacao

--- app/Anotacao.py
+++ app/Anotacao.py
@@ -1,11 +1,12 @@
 from typing import Optional
 from datetime import date
 
+'''A Classe Anotação permite registrar e listar anotações relacionadas à uma publicação '''
 class Anotacao:
     def __init__ (self, texto, trecho = Optional[str], data = date):
         self.texto = texto
         self.trecho = trecho
-        data = data
+        self.data = data
     
     def adicionar_anotacao(self, texto: str, trecho: Optional[str] = None):
         pass
