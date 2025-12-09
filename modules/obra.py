d5c8c56c4aaf85f7e6fd342cda2c3a6bb166ef0b
sarahmendes-ufca <sarah.mendes@aluno.ufca.edu.br>
2025-12-09 15:59:13 -0300

    Update obra.py

--- modules/obra.py
+++ modules/obra.py
@@ -1,89 +1,59 @@
-class Obra:
-    def __init__(self, titulo, ano, autor, tipo, numero_paginas, avaliacao):
+from abc import ABC, abstractmethod
+class Obra(ABC):
+    def __init__(self, titulo, ano, tipo, numero_paginas, avaliacao = None):
         self.titulo = titulo
         self.ano = ano
-        self.autor = autor
         self.tipo = tipo
         self.numero_paginas = numero_paginas
         self.avaliacao = avaliacao
 
     @property
+    @abstractmethod
     def titulo(self):
-        print("Título da obra: ")
-        return self._titulo
+        pass
     
     @titulo.setter
+    @abstractmethod
     def titulo(self, titulo_valido: str):
-        if titulo_valido.strip() == "":
-            raise ValueError("Título inválido! O campo não pode ser vazio.")
-        else:
-            self._titulo = titulo_valido     
+        pass    
 
     @property
     def ano(self):
-        print("Ano de publicação da obra: ")
-        return self._ano
+        pass
     
     @ano.setter
+    @abstractmethod
     def ano(self, ano_valido):
-        if ano_valido < 1500:
-            raise ValueError("Ano inálido! O ano deve ser maior que 1500!")
-        else:
-            self._ano = ano_valido
-    @property
-    def autor(self):
-        print("Autor da Obra: ")
-        return self._autor
-    
-    @autor.setter
-    def autor(self, autor_valido):
-        if autor_valido == "":
-            raise ValueError("Autor inválido! O campo não pode ser vazio!")
-        else: 
-            self._autor = autor_valido
+        pass
+
         
     @property
+    @abstractmethod
     def tipo(self):
-        print("Tipo da obra")
-        return self._tipo
+        pass
     
     @tipo.setter 
+    @abstractmethod
     def tipo(self, tipo_valido):
-        if tipo_valido not in ("livro", "revista"):
-            raise ValueError("Tipo inválido! O tipo deve ser livro ou revista!")
-        else:
-            self._tipo = tipo_valido
+        pass
         
     @property
+    @abstractmethod
     def numero_paginas(self):
-        print("Número de páginas")
-        return self._numero_paginas
+        pass
     
     @numero_paginas.setter
+    @abstractmethod
     def numero_paginas(self, numero_paginas_valido):
-        if isinstance(numero_paginas_valido, int):
-            self._numero_paginas = numero_paginas_valido
-        else:
-            return("Número de páginas inválido!")
+        pass
 
     @property
+    @abstractmethod
     def avaliacao(self):
-        print("Avaliação da Obra: ")
-        return self._avaliacao
+        pass
     
     @avaliacao.setter
+    @abstractmethod
     def avaliacao(self, avaliacao_valido):
-        if isinstance(avaliacao_valido, float):
-            self._avaliacao = avaliacao_valido
-        else:
-            return("Avaliação inválida!") 
-        
-    # Testes: Criação de instâncias de classe
-
-pub = Obra("Dom Casmurro", 1500, "Machado_de_Assis", "livro", 280, 8.9)
-print(pub.titulo)
-print(pub.ano)
-print(pub.autor)
-print(pub.tipo)
-print(pub.numero_paginas)
-print(pub.avaliacao)
+        pass
+    
