b40581793fabd92e8449e866db97d3337723db3c
sarahmendes-ufca <sarah.mendes@aluno.ufca.edu.br>
2025-12-09 16:01:15 -0300

    Update revista.py

--- modules/revista.py
+++ modules/revista.py
@@ -7,16 +7,49 @@ class Revista(Obra):
         self.genero = genero
 
     @property
+    def titulo(self):
+        return self._titulo
+    
+    @titulo.setter
+    def titulo(self, titulo_valido: str):
+        if titulo_valido.strip() == "":
+            raise ValueError("Título inválido! O campo não pode ser vazio.")
+        else:
+            self._titulo = titulo_valido
+
+    @property
+    def ano(self):
+        return self._ano
+    
+    @ano.setter
+    def ano(self, ano_valido):
+        if ano_valido < 1500:
+            raise ValueError("Ano inálido! O ano deve ser maior que 1500!")
+        else:
+            self._ano = ano_valido
+
+    @property
     def editora(self):
-        print("Autor da Obra: ")
+        print("Editora da Revista: ")
         return self._editora
     
     @editora.setter
-    def autor(self, editora_valida):
+    def editora(self, editora_valida):
         if editora_valida == "":
             raise ValueError("Editora inválida! O campo não pode ser vazio!")
         else: 
             self._editora = editora_valida
+
+    @property
+    def tipo(self):
+        return self._tipo
+    
+    @tipo.setter 
+    def tipo(self, tipo_valido):
+        if tipo_valido not in ("revista", "livro"):
+            raise ValueError("Tipo inválido! O tipo deve ser livro ou revista!")
+        else:
+            self._tipo = tipo_valido
 
     @property
     def genero(self):
@@ -30,11 +63,29 @@ class Revista(Obra):
         else:
             self._genero = genero_valido
 
-livro = Revista("Dom Casmurro", 1888, "Machado_de_Assis", "livro","epico", 280, 8.9)
-print(livro.titulo)
-print(livro.ano)
-print(livro.autor)
-print(livro.tipo)
-print(livro.genero)
-print(livro.numero_paginas)
-print(livro.avaliacao)    
+    @property
+    def numero_paginas(self):
+        print("Número de páginas")
+        return self._numero_paginas
+    
+    @numero_paginas.setter
+    def numero_paginas(self, numero_paginas_valido):
+        if not isinstance(numero_paginas_valido, int):
+            raise ValueError("Erro. O número de páginas deve ser um valor inteiro.")
+        else:
+            self._numero_paginas = numero_paginas_valido
+
+    @property
+    def avaliacao(self):
+        print("Avaliação da Obra: ")
+        return self._avaliacao
+    
+    @avaliacao.setter
+    def avaliacao(self, avaliacao_valido):
+        if not avaliacao_valido == None:
+            if not isinstance(avaliacao_valido, float) or avaliacao_valido > 10 or avaliacao_valido < 0:
+                raise ValueError("Nota inválida! Deve ser entre 0-10!")
+            else:
+                self._avaliacao = avaliacao_valido 
+        else:
+            self._avaliacao = "Campo vazio"
